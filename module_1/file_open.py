042892d7cc9aebab30e5c12a331515a4a2cd8a81
Austin Morris <157867780+amorrisCS@users.noreply.github.com>
2024-04-12 16:07:36 -0500

    Update file_open.py
    Comment added for test case 1.

--- module_1/file_open.py
+++ module_1/file_open.py
@@ -5,7 +5,8 @@ import json
 # Read in file with a filename and return links 
 # Single Responsibility Principle for the benefit of improved readiblity, and cohesion with
 # the rest of the program (this module ONLY reads in the links and returns them)
-def read_file():            
+def read_file():  
+    # Test case 1 - check if file exists
     try:
         filename = input("Enter the filename: ")
         # Set up input folder and process
